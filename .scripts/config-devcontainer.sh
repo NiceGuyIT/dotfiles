3e088eddc4da4093bf5573419f794ee983b74eba
David Randall <David@NiceGuyIT.biz>
2025-11-13 21:29:37 -0500

    feat: Sync devcontainers and config
    Signed-off-by: David Randall <David@NiceGuyIT.biz>

--- .scripts/config-devcontainer.sh
+++ .scripts/config-devcontainer.sh
@@ -4,6 +4,20 @@
 # https://containers.dev/implementors/json_reference/
 # This is run as root.
 
+# Add the user to the host's docker group
+sudoIf() { if [ "$(id -u)" -ne 0 ]; then sudo "$@"; else "$@"; fi }
+NONROOT_USER=dev
+SOCKET_GID=$(stat -c '%g' /var/run/docker.sock)
+if [ "${SOCKET_GID}" != '0' ]; then
+  if [ "$(grep :${SOCKET_GID}: /etc/group)" = '' ]; then
+    sudoIf groupadd --gid ${SOCKET_GID} docker-host;
+  fi
+
+  if [ "$( id ${NONROOT_USER} | grep -E "groups=.*(=|,)${SOCKET_GID}\(" )" = '' ]; then
+    sudoIf usermod --append --group ${SOCKET_GID} ${NONROOT_USER};
+  fi
+fi
+
 # Install Prettier and cspell
 [[ ! -e ".bun/bin/prettier" ]] && /usr/local/bin/bun install --global prettier
 [[ ! -e ".bun/bin/cspell" ]] && /usr/local/bin/bun install --global cspell
@@ -18,10 +32,19 @@ if [[ -d /.jbdevcontainer/ ]]
 then
 
   # Chezmoi uses $XDG_CONFIG_HOME for the config directory and $XDG_DATA_HOME for the data directory.
-  [[ ! -e /.jbdevcontainer/config/chezmoi ]] && ln -s ~/.config/chezmoi /.jbdevcontainer/config/
-  [[ ! -e /.jbdevcontainer/data/chezmoi ]] && ln -s ~/.config/chezmoi /.jbdevcontainer/data/
-  
+  if [[ -d /.jbdevcontainer/config/ ]] && [[ ! -e /.jbdevcontainer/config/chezmoi ]]
+  then
+    ln -s ~/.config/chezmoi /.jbdevcontainer/config/
+  fi
+  if [[ -d /.jbdevcontainer/data/ ]] && [[ ! -e /.jbdevcontainer/data/chezmoi ]]
+  then
+    ln -s ~/.config/chezmoi /.jbdevcontainer/data/
+  fi
+
   # Nushell uses $XDG_CONFIG_HOME for the config directory.
-  [[ ! -e /.jbdevcontainer/config/nushell ]] && ln -s ~/.config/nushell /.jbdevcontainer/config/
+  if [[ -d /.jbdevcontainer/config/ ]] && [[ ! -e /.jbdevcontainer/config/nushell ]]
+  then
+    ln -s ~/.config/nushell /.jbdevcontainer/config/
+  fi
 
 fi
